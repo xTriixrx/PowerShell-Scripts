7987bbf15076d4455cf43f993862d161adfe7169
Vincent Nigro <48078212+xTriixrx@users.noreply.github.com>
2019-11-01 12:35:06 -0400

    v0.0.1

---
+++ Get-Permissions.ps1
@@ -0,0 +1,99 @@
+﻿<#
+
+.SYNOPSIS
+Permission seeking utility with recursive capabilities.
+
+.DESCRIPTION
+This tool is used to be allowed to recursively get information down a Windows file system in a depth-first manner.
+This tool requires extreme caution when using, because some edge cases can cause the depth-first search to recurse
+into a infinite loop; really meaning when the file system bottoms out.
+
+This issue is still being considered on ways to address the fix, as well as the required -Init field. Which may seem
+ambigious, however it controls the depth order of the recursive function correctly. 
+
+Note: -Init will ALWAYS be set during the instantiation of this recursive call. DO NOT EVER attempt to call Get-Permission
+with -Init set to $False. This will potentially produce the edge case as discussed above.
+
+Calling Get-Permission requires the following field:
+    -Folder [relative/path]
+    -Depth_Set [Int]
+    -Init $True
+
+This recursive call when used correctly will return a depth-first manner of the directory passed and "diving" as deep
+as what -Depth_Set is assigned to. Any directories with a depth less than the -Depth_Set will move back up the recurse 
+list faster.
+
+For each iteration, this tool will show the current depth, the location of the dive, the identies of the users who
+have access, as well as the variety of permissions, access, and flags associated with that user.
+
+.EXAMPLE
+Get-Permissions -Folder folderName -Depth_Set 1 -Init $True
+.EXAMPLE
+Get-Permissions -Folder local\folder -Depth_Set 2 -Init $True
+
+.NOTES
+Author: Vincent Nigro
+Last Updated: 11/1/2019
+Version: 0.0.1
+
+.LINK
+https://github.com/xTriixrx
+
+#>
+function Get-Permissions
+{
+    param(
+        [Parameter(Mandatory=$True)]
+        [ValidateNotNull()]
+        [String]
+        $Folder,
+        [Parameter(Mandatory=$True)]
+        [ValidateNotNull()]
+        [Int]
+        $Depth_Set,
+        [Parameter(Mandatory=$True)]
+        [ValidateNotNull()]
+        [Boolean]
+        $Init,
+        [Parameter(Mandatory=$False)]
+        [ValidateNotNull()]
+        [Int]
+        $Depth
+        )
+
+        if($Init -eq $True -and $Depth) {
+            Write-Output "InvalidParameterException: -Depth should not be set during recursive initialization (-Init). See Get-Help Get-Permissions for more information." 
+            return
+        }
+        
+        if($Init) {
+            $Depth = 0;
+        }
+        else {
+            $Depth = $Depth + 1;
+        }
+
+    Foreach ($Item in Get-ChildItem $Folder) {
+        #$TAB = "`t" * $Depth;
+
+        Write-Output "Depth: $Depth" " ";
+
+        $Location = $Folder + '\' + $Item;
+        Write-Output "Location" -------- $Location;  
+
+        (get-acl $Location).access | select `
+		    @{Label="Identity";Expression={$_.IdentityReference}}, `
+		    @{Label="Right";Expression={$_.FileSystemRights}}, `
+		    @{Label="Access";Expression={$_.AccessControlType}}, `
+		    @{Label="Inherited";Expression={$_.IsInherited}}, `
+		    @{Label="Inheritance Flags";Expression={$_.InheritanceFlags}}, `
+		    @{Label="Propagation Flags";Expression={$_.PropagationFlags}} | ft -auto
+
+        Write-Output " " " " " ";
+        if ($Depth -ne $Depth_Set) {
+            if (Test-Path $Location -PathType Container) {
+                Get-Permissions -Folder $Location -Depth_Set $Depth_Set -Init $False -Depth $Depth;
+            }
+        }
+    } # End of Foreach
+}
---
+++ Jar-Search.ps1
@@ -0,0 +1,102 @@
+﻿<#
+
+.SYNOPSIS
+Tool for tracking and searching jar files and inner components in a Windows file system.
+
+.DESCRIPTION
+Jar-Search currently has 4 different sets of functionality captured by the required parameter -Stype:
+    - Search All        (SALL)
+    - List Classes      (LCLS)
+    - List Local Jars   (LLJR)
+    - List System Jars  (LSJR)
+
+All parameters (-Stype, -Class, -Jarfile, -Drives) are passed as strings, ensure to enclose with " " to ensure safety.
+
+Search All (SALL) is dependant on -Class being passed a class name that may exist within a set of .jar files.
+
+List Classes (LCLS) is dependant on -Jarfile being passed a jar file name that does exist within the current working directory.
+    Note: This function does not have safety checking for if a file exists within the directory.
+
+List Local Jars (LLJR) is not dependant on any other parameters and just returns the list of any jar files in the current working directory.
+
+List System Jars (LSJR) is dependant on -Drives being passed 1 or more locations to search recursively from (such as a C or D drive).
+    Note: This function does not have safety checking for if a file system exists before checking, be sure that the file systems passed exist.
+
+.EXAMPLE
+Jar-Search -Stype "SALL" -Class "ClassToSearch"
+
+ClassToSearch is any class name without the .class extension.
+.EXAMPLE
+Jar-Search -Stype "LCLS" -Jarfile "somefilethatendswith.jar"
+.EXAMPLE
+Jar-Search -Stype "LLJR"
+.EXAMPLE
+Jar-Search -Stype "LSJR" -Drives "c:\"
+.EXAMPLE
+Jar-Search -Stype "LSJR" -Drives "c:\ d:\"
+Note: Be sure to add space between multiple drives and following the colon with a backslash.
+
+.NOTES
+Author: Vincent Nigro
+Last Updated: 11/1/2019
+Version: 0.0.1
+
+.LINK
+https://github.com/xTriixrx
+
+#>
+function Jar-Search
+{
+    param(
+        [Parameter(Mandatory=$True)]
+        [ValidateNotNull()]
+        [String]
+        $Stype,
+        [Parameter(Mandatory=$False)]
+        [ValidateNotNull()]
+        [String]
+        $Class,
+        [Parameter(Mandatory=$False)]
+        [ValidateNotNull()]
+        [String]
+        $Jarfile,
+        [Parameter(Mandatory=$False)]
+        [ValidateNotNull()]
+        [String]
+        $Drives
+    )
+
+    $DATE = (Get-Date).ToString("MM-dd-yyyy");
+    $TMPFILE =  $DATE + "_tmp.txt"
+    $CMD = ""
+    switch($Stype) {
+        "SALL" {
+            if (!$Class) {
+                Write-Output "InvalidParameterException: Search All (SALL) requires -Class. 'Get-Help Jar-Search' for more information."
+                return
+            }
+            $CMD = "forfiles /S /M *.jar /C `"cmd /c jar -tvf @file | findstr /C:`"" + $Class + "`" && echo @path`" > " + $TMPFILE      
+        }
+        "LCLS" {
+            if (!$Jarfile) {
+                Write-Output "InvalidParameterException: List Classes (LCLS) requires -Jarfile. 'Get-Help Jar-Search' for more information."
+                return
+            }
+            $CMD = "jar -tvf " + $Jarfile + " | findstr /C:`"`.class`" > " + $TMPFILE
+            
+        }
+        "LLJR" {
+            $CMD = "dir /S /b *.jar > " + $TMPFILE
+        }
+        "LSJR" {
+            if (!$Drives) {
+                Write-Output "InvalidParameterException: List System Jars (LSJR) requires -Drives. 'Get-Help Jar-Search' for more information."
+                return
+            }
+            $CMD = "dir " + $Drives + " /S /b | findstr /E /C:`".jar`" > " + $TMPFILE        
+        }
+    }
+    cmd /c $CMD
+    Get-Content -Path $TMPFILE
+    Remove-Item $TMPFILE
+}
